f440ff4fc218a0f5077b1d6f69bd162c4c0e0f12
Ike Devolder <ike.devolder@studioemma.eu>
2015-09-25 16:36:10 +0200

    bashrc and aliases install
    Signed-off-by: Ike Devolder <ike.devolder@studioemma.eu>

--- magento2.sh
+++ magento2.sh
@@ -9,6 +9,26 @@ apt-get update
 echo "Europe/Brussels" > /etc/timezone
 dpkg-reconfigure --frontend noninteractive tzdata 2>&1
 
+# bashrc
+tail -n 10 /etc/skel/.bashrc >> /etc/bashrc
+
+install -Dm644 /vagrant/manifests/bashrc/bashrc /etc/bash.bashrc.local
+printf "\n[ -e /etc/bash.bashrc.local ] && . /etc/bash.bashrc.local\n" \
+    >> /etc/bash.bashrc
+
+echo 'PSCOL=${BLD}${COLWHT}' > /etc/bashrc.config
+echo 'USRCOL=${BLD}${COLCYN}' >> /etc/bashrc.config
+echo 'HSTCOL=${BLD}${COLRED}' >> /etc/bashrc.config
+echo 'SCMENABLED=0' >> /etc/bashrc.config
+echo 'SCMDIRTY=0' >> /etc/bashrc.config
+
+tail -n 19 /etc/skel/.bashrc | head -n 8 > /root/.bashrc
+tail -n 19 /etc/skel/.bashrc | head -n 8 > /home/vagrant/.bashrc
+
+install -Dm644 /vagrant/manifests/bash/bash_aliases \
+    /home/vagrant/.bash_aliases
+chown --reference /home/vagrant /home/vagrant/.bash_aliases
+
 # default packages
 apt-get install -y vim htop curl git-core ant python-software-properties
 
