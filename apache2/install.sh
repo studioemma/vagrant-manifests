b72fa64f8801cca7f2f8a130efd305c417a5339b
Ike Devolder <ike.devolder@studioemma.eu>
2015-09-28 09:43:29 +0200

    split apache2 install
    Signed-off-by: Ike Devolder <ike.devolder@studioemma.eu>

---
+++ apache2/install.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+apache_basedir=$(dirname $(readlink -f $0))
+
+set -e
+
+# apache
+apt-get install -y apache2
+
+a2dismod mpm_prefork
+a2dismod mpm_event
+a2dismod auth_basic
+a2dismod authn_file
+
+a2enmod mpm_worker
+a2enmod proxy_fcgi
+
+a2enmod rewrite
+a2enmod headers
+a2enmod deflate
+a2enmod alias
+a2enmod status
+
+# create log folder in case it does not exist
+mkdir -p /var/www/website/var/log
+
+# install config
+rm -f /etc/apache2/sites-enabled/*
+cp files/website.conf /etc/apache2/sites-enabled/
+
+service apache2 restart
+
