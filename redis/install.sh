67e491a8caccdefa68f61d6a2b5a65aa1326c518
Ike Devolder <ike.devolder@studioemma.eu>
2015-09-28 11:51:35 +0200

    redis, if php is there install php redis
    Signed-off-by: Ike Devolder <ike.devolder@studioemma.eu>

--- redis/install.sh
+++ redis/install.sh
@@ -10,6 +10,14 @@ add-apt-repository -y ppa:chris-lea/redis-server
 apt-get update
 apt-get install -y redis-server
 
+if which php > /dev/null 2>&1; then
+    pecl install redis
+    echo "extension=redis.so" > /etc/php5/mods-available/redis.ini
+    php5enmod redis
+
+    service php5-fpm restart
+fi
+
 service redis-server restart
 
 cd "$redis_calldir"
