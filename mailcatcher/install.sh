f5fb90dbd85eb77e7456650b236ebcfd482b6c36
Ike Devolder <ike.devolder@studioemma.eu>
2015-09-28 11:54:13 +0200

    mailcatcher, cleanup php install a bit
    Signed-off-by: Ike Devolder <ike.devolder@studioemma.eu>

--- mailcatcher/install.sh
+++ mailcatcher/install.sh
@@ -11,18 +11,17 @@ apt-get install -y build-essential libsqlite3-dev ruby-dev
 
 gem install mailcatcher
 
-if which php > /dev/null 2>&1; then
-    echo "sendmail_path = /usr/bin/env $(which catchmail)" \
-        >> /etc/php5/mods-available/mailcatcher.ini
-    php5enmod mailcatcher
-fi
-
 install -Dm644 files/mailcatcher.upstart.conf \
     /etc/init/mailcatcher.conf
 
 if which php > /dev/null 2>&1; then
+    echo "sendmail_path = /usr/bin/env $(which catchmail)" \
+        >> /etc/php5/mods-available/mailcatcher.ini
+    php5enmod mailcatcher
+
     service php5-fpm restart
 fi
+
 service mailcatcher restart
 
 cd "$mailcatcher_calldir"
