920ea40781c985e1b67ecf93e3fab631ca2c0e9d
Ike Devolder <ike.devolder@studioemma.eu>
2015-09-29 14:11:02 +0200

    magento2, set noninteractive and use 'modules' for all
    Signed-off-by: Ike Devolder <ike.devolder@studioemma.eu>

--- magento2.sh
+++ magento2.sh
@@ -10,23 +10,17 @@ fi
 
 set -e
 
-# get up2date list of packages in apt
-apt-get update
-
-# set timezone
-echo "Europe/Brussels" > /etc/timezone
-dpkg-reconfigure --frontend noninteractive tzdata 2>&1
-
-# default packages
-apt-get install -y vim htop curl git-core ant python-software-properties
+export DEBIAN_FRONTEND=noninteractive
 
 # install 'modules'
+init/install.sh
 bash/install.sh
 apache2/install.sh
 php/install.sh
-mysql/install-5.6.sh
+mysql/install-oracle.sh
 grunt/install.sh
 mailcatcher/install.sh
+magento/install.sh
 
 if [ "/tmp" != "$basedir" ]; then
     cd "$calldir"
