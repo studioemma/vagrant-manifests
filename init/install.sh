af4f551f0a111c734b8e5877255a2bcc32091bc0
Ike Devolder <ike.devolder@studioemma.eu>
2015-09-29 12:33:06 +0200

    add 'init' module with some basic needs
    Signed-off-by: Ike Devolder <ike.devolder@studioemma.eu>

---
+++ init/install.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+init_basedir=$(dirname $(readlink -f $0))
+init_calldir=$(pwd)
+
+cd "$init_basedir"
+
+set -e
+
+# get up2date list of packages in apt
+apt-get update
+
+# set timezone
+echo "Europe/Brussels" > /etc/timezone
+dpkg-reconfigure --frontend noninteractive tzdata 2>&1
+
+# default packages
+apt-get install -y vim htop curl git-core ant python-software-properties
+
+cd "$init_calldir"
