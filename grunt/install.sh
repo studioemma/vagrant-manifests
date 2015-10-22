84eb114f1764c86852e6ffe1ce6d79631a100d3a
Ike Devolder <ike.devolder@studioemma.eu>
2015-10-22 11:15:11 +0200

    grunt now uses nodejs 4.x
    Signed-off-by: Ike Devolder <ike.devolder@studioemma.eu>

--- grunt/install.sh
+++ grunt/install.sh
@@ -7,18 +7,9 @@ cd "$grunt_basedir"
 set -e
 
 # install grunt
-add-apt-repository -y ppa:chris-lea/node.js
-apt-get update
+curl -sL https://deb.nodesource.com/setup_4.x | bash -
 apt-get install -y nodejs
 
 npm install -g grunt-cli
-npm install -g underscore
-npm install -g node-minify
-npm install -g glob
-npm install -g time-grunt
-npm install -g load-grunt-config
-npm install -g imagemin-svgo
-npm install -g jit-grunt
-npm install -g grunt-contrib-watch
 
 cd "$grunt_calldir"
