f25921e4cadb7b86a2b5b41d7f9fe615f4bd5a1a
Ike Devolder <ike.devolder@studioemma.eu>
2015-11-09 09:55:08 +0100

    magento :: add MAGE_MODE env var for cli
    Signed-off-by: Ike Devolder <ike.devolder@studioemma.eu>

--- magento/install.sh
+++ magento/install.sh
@@ -9,6 +9,9 @@ set -e
 # magento2 specific stuff
 install -Dm644 files/cron.magento2 /etc/cron.d/magento2
 
+# set env var for cli tools
+echo "export MAGE_MODE=developer" >> /home/vagrant/.bashrc
+
 if which php > /dev/null 2>&1; then
     echo "env[MAGE_MODE] = developer" >> /etc/php5/fpm/pool.d/www.conf
     service php5-fpm restart
