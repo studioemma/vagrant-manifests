b1e1c04cc50f351cf567bf81cb8b3fbfa08237a6
Ike Devolder <ike.devolder@studioemma.eu>
2015-11-13 15:54:57 +0100

    deploy :: change to custom deploy
    Signed-off-by: Ike Devolder <ike.devolder@studioemma.eu>

---
+++ deploy/files/clone.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [[ "vagrant" == "$(whoami)" ]]; then
+    cd "$HOME"
+    ssh-keyscan -p7999 stash.studioemma.com >> "$HOME/.ssh/known_hosts"
+
+    mkdir -p "$HOME/.bin/tools"
+    cd "$HOME/.bin/tools"
+
+    git clone ssh://git@stash.studioemma.com:7999/mag2/deploy.git
+
+    cd "$HOME/.bin"
+    ln -s tools/deploy/bin/deploy
+
+    echo '#!/bin/bash' > "$HOME/.bin/deploy-self-update"
+    echo '(' > "$HOME/.bin/deploy-self-update"
+    echo 'cd $HOME/.bin/tools/deploy' >> "$HOME/.bin/deploy-self-update"
+    echo 'git pull' >> "$HOME/.bin/deploy-self-update"
+    echo ')' >> "$HOME/.bin/deploy-self-update"
+
+    chmod +x "$HOME/.bin/deploy-self-update"
+fi
--- deploy/install.sh
+++ deploy/install.sh
@@ -6,6 +6,6 @@ cd "$deploy_basedir"
 
 set -e
 
-su vagrant -c files/magallanes.sh
+su vagrant -c files/clone.sh
 
 cd "$deploy_calldir"
