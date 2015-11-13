9b2d55dc4d3d6b96a21dd6fdc9450f6938ee2989
Ike Devolder <ike.devolder@studioemma.eu>
2015-11-13 10:45:04 +0100

    package :: fix self-update
    Signed-off-by: Ike Devolder <ike.devolder@studioemma.eu>

--- package/files/clone.sh
+++ package/files/clone.sh
@@ -14,9 +14,9 @@ if [[ "vagrant" == "$(whoami)" ]]; then
 
     echo '#!/bin/bash' > "$HOME/.bin/package-self-update"
     echo '(' > "$HOME/.bin/package-self-update"
-    echo 'cd $HOME/.bin/tools/package' > "$HOME/.bin/package-self-update"
-    echo 'git pull' > "$HOME/.bin/package-self-update"
-    echo ')' > "$HOME/.bin/package-self-update"
+    echo 'cd $HOME/.bin/tools/package' >> "$HOME/.bin/package-self-update"
+    echo 'git pull' >> "$HOME/.bin/package-self-update"
+    echo ')' >> "$HOME/.bin/package-self-update"
 
     chmod +x "$HOME/.bin/package-self-update"
 fi
