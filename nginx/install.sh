7371a704179e046402211c6992b4474a6a9e91d3
Ike Devolder <ike.devolder@studioemma.eu>
2015-12-01 14:53:46 +0100

    nginx :: don't chown vagrant:vagrant
    Signed-off-by: Ike Devolder <ike.devolder@studioemma.eu>

--- nginx/install.sh
+++ nginx/install.sh
@@ -19,8 +19,6 @@ install -Dm644 files/website.conf /etc/nginx/sites-enabled/
 # run nginx as vagrant user
 sed -e 's/^user.*/user vagrant;/' -i /etc/nginx/nginx.conf
 
-chown -R vagrant:vagrant /var/website
-
 service nginx restart
 
 cd "$nginx_calldir"
