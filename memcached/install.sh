3326f4dd6e32096e3e8a2ab2224425447bac2d62
BlackEagle <ike.devolder@gmail.com>
2016-05-23 20:06:53 +0200

    memcached :: update to support php 5.5, 5.6 and 7.0
    Signed-off-by: BlackEagle <ike.devolder@gmail.com>

--- memcached/install.sh
+++ memcached/install.sh
@@ -9,18 +9,11 @@ set -e
 apt-get install -y memcached
 
 if which php > /dev/null 2>&1; then
-    phpversion=$(php -v | sed -rn 's/PHP ([0-9]{1}).*/\1/p')
-    if [[ $phpversion -eq 7 ]]; then
-        apt-get install -y php-memcached
+    phpversion=$(php -v | sed -rn 's/PHP ([0-9]+\.[0-9]+).*/\1/p')
 
-        service php7.0-fpm restart
-    else
-        apt-get install -y php5-memcache php5-memcached
-        php5enmod memcache
-        php5enmod memcached
+    apt-get install -y php${phpversion}-memcached
 
-        service php5-fpm restart
-    fi
+    service php${phpversion}-fpm restart
 
     # install memcacheadmin
     ( cd /var/www; git clone https://github.com/hgschmie/phpmemcacheadmin.git )
