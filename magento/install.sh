e7b56b39a8cae63a640dcad28a7faed08496fe76
BlackEagle <ike.devolder@gmail.com>
2016-05-23 20:02:06 +0200

    magento :: updated phpversion and so simplify update and restart
    Signed-off-by: BlackEagle <ike.devolder@gmail.com>

--- magento/install.sh
+++ magento/install.sh
@@ -10,16 +10,9 @@ set -e
 install -Dm644 files/cron.magento2 /etc/cron.d/magento2
 
 if which php > /dev/null 2>&1; then
-    phpversion=$(php -v | sed -rn 's/PHP ([0-9]{1}).*/\1/p')
-    if [[ $phpversion -eq 7 ]]; then
-        echo "env[MAGE_MODE] = developer" >> /etc/php/7.0/fpm/pool.d/www.conf
-
-        service php7.0-fpm restart
-    else
-        echo "env[MAGE_MODE] = developer" >> /etc/php5/fpm/pool.d/www.conf
-
-        service php5-fpm restart
-    fi
+    phpversion=$(php -v | sed -rn 's/PHP ([0-9]+\.[0-9]+).*/\1/p')
+    echo "env[MAGE_MODE] = developer" >> /etc/php/$phpversion/fpm/pool.d/www.conf
+    service php${phpversion}-fpm restart
 fi
 
 cd "$magento_calldir"
