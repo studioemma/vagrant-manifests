3bb8db24483b84dd287bf8396a57aadb987d9edd
Ike Devolder <ike.devolder@studioemma.eu>
2017-01-06 12:18:28 +0100

    nodejs :: update for xenial
    Signed-off-by: Ike Devolder <ike.devolder@studioemma.eu>

--- nodejs/install.sh
+++ nodejs/install.sh
@@ -7,7 +7,7 @@ cd "$nodejs_basedir"
 set -e
 
 # install nodejs
-curl -sL https://deb.nodesource.com/setup_4.x | bash -
-apt-get install -y nodejs
+curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
+sudo apt-get install -y nodejs
 
 cd "$nodejs_calldir"
