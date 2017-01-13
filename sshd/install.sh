b857428bc4a551b4697468e3da8c7a8a75eac874
BlackEagle <ike.devolder@gmail.com>
2017-01-13 14:08:06 +0100

    sshd :: only add insecure key when authorized_keys is empty
    Signed-off-by: BlackEagle <ike.devolder@gmail.com>

--- sshd/install.sh
+++ sshd/install.sh
@@ -11,9 +11,19 @@ sed -e 's/\(.*AcceptEnv.*\)/# \1/' -i /etc/ssh/sshd_config
 systemctl restart ssh
 
 # insert the insecure key
-wget https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub -O /home/ubuntu/.ssh/authorized_keys
-chmod 700 /home/ubuntu/.ssh
-chmod 600 /home/ubuntu/.ssh/authorized_keys
-chown -R ubuntu:ubuntu /home/ubuntu/.ssh
+addinsecurekey=0
+if [[ ! -e "/home/ubuntu/.ssh/authorized_keys" ]]; then
+    addinsecurekey=1
+elif [[ "0" == "$(cat /home/ubuntu.ssh/authorized_keys | wc -l)" ]]; then
+    addinsecurekey=1
+fi
+
+if [[ 1 == $addinsecurekey ]]; then
+    wget https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub \
+        -O /home/ubuntu/.ssh/authorized_keys
+    chmod 700 /home/ubuntu/.ssh
+    chmod 600 /home/ubuntu/.ssh/authorized_keys
+    chown -R ubuntu:ubuntu /home/ubuntu/.ssh
+fi
 
 cd "$sshd_calldir"
