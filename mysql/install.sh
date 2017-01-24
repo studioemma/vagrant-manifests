243d6cd9c247111ac3c10aa136d6e83b73a1a413
BlackEagle <ike.devolder@gmail.com>
2017-01-24 16:12:48 +0100

    mysql :: fix mysql not using our se config
    Signed-off-by: BlackEagle <ike.devolder@gmail.com>

--- mysql/install.sh
+++ mysql/install.sh
@@ -14,11 +14,11 @@ echo 'mysql-server  mysql-server/root_password_again password toor' \
 
 apt-get install -y mysql-server mysql-client
 
-echo "[mysqld]" > /etc/mysql/conf.d/se.cnf
-echo "sql_mode=NO_ENGINE_SUBSTITUTION" >> /etc/mysql/conf.d/se.cnf
-echo "performance_schema = 0" >> /etc/mysql/conf.d/se.cnf
-echo "bind-address = 0.0.0.0" >> /etc/mysql/conf.d/se.cnf
-echo "innodb_doublewrite = 0" >> /etc/mysql/conf.d/se.cnf # increase perf in dev
+echo "[mysqld]" > /etc/mysql/mysql.conf.d/se.cnf
+echo "sql_mode=NO_ENGINE_SUBSTITUTION" >> /etc/mysql/mysql.conf.d/se.cnf
+echo "performance_schema = 0" >> /etc/mysql/mysql.conf.d/se.cnf
+echo "bind-address = 0.0.0.0" >> /etc/mysql/mysql.conf.d/se.cnf
+echo "innodb_doublewrite = 0" >> /etc/mysql/mysql.conf.d/se.cnf # increase perf in dev
 
 systemctl stop mysql
 sleep 2
@@ -26,10 +26,5 @@ systemctl start mysql
 
 mysql -uroot -ptoor -e \
     "grant all on *.* to 'root'@'%' identified by 'toor'; flush privileges;"
-
-# create a default magento2 database
-# @TODO remove not needed
-#mysql -uroot -ptoor -e \
-    #"CREATE DATABASE magento2 DEFAULT CHARSET utf8;"
 
 cd "$mysql_calldir"
