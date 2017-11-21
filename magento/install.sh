ac1bbea47b31e1e3199fed783c83918a860156bd
BlackEagle <ike.devolder@gmail.com>
2017-11-21 13:43:17 +0100

    dont set MAGE_MODE externally, allow env.php to determine the MAGE_MODE
    Signed-off-by: BlackEagle <ike.devolder@gmail.com>

--- magento/install.sh
+++ magento/install.sh
@@ -9,12 +9,6 @@ set -e
 # magento2 specific stuff
 install -Dm644 files/cron.magento2 /etc/cron.d/magento2
 
-if which php > /dev/null 2>&1; then
-    phpversion=$(php -v | sed -rn 's/PHP ([0-9]+\.[0-9]+).*/\1/p')
-    echo "env[MAGE_MODE] = developer" >> /etc/php/$phpversion/fpm/pool.d/www.conf
-    service php${phpversion}-fpm restart
-fi
-
 # install magerun
 wget https://files.magerun.net/n98-magerun2-latest.phar -O /usr/local/bin/magerun
 chmod +x /usr/local/bin/magerun
