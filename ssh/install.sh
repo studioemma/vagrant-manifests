9a3d6986132e15f4f54b9707f0eaca85e6100403
BlackEagle <ike.devolder@gmail.com>
2018-06-04 10:16:42 +0200

    latest ubuntu images use vagrant user again, use vagrant user ssh
    Signed-off-by: BlackEagle <ike.devolder@gmail.com>

--- ssh/install.sh
+++ ssh/install.sh
@@ -6,6 +6,6 @@
 
 #[[ ! -e "$startdir/id_rsa.pub" ]] && echo "please add your public key as id_rsa.pub" && exit 3
 
-cp -a "$1"/id_rsa /home/ubuntu/.ssh/
-chown ubuntu:ubuntu /home/ubuntu/.ssh/id_rsa
-chmod 0600 /home/ubuntu/.ssh/id_rsa
+cp -a "$1"/id_rsa /home/vagrant/.ssh/
+chown vagrant:vagrant /home/vagrant/.ssh/id_rsa
+chmod 0600 /home/vagrant/.ssh/id_rsa
