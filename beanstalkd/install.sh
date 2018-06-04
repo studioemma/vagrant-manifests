c845ebd7fc0117d38472e1b576fe312a46cf71c6
BlackEagle <ike.devolder@gmail.com>
2018-06-04 10:14:22 +0200

    latest ubuntu images use vagrant user again, use vagrant user for beanstalkd admin
    Signed-off-by: BlackEagle <ike.devolder@gmail.com>

--- beanstalkd/install.sh
+++ beanstalkd/install.sh
@@ -11,7 +11,7 @@ apt-get install -y beanstalkd
 if which php > /dev/null 2>&1; then
     # install phpbeanstalkdadmin
     ( cd /var/www; git clone https://github.com/mnapoli/phpBeanstalkdAdmin.git phpbeanstalkdadmin )
-    chown ubuntu:ubuntu -R /var/www/phpbeanstalkdadmin
+    chown vagrant:vagrant -R /var/www/phpbeanstalkdadmin
     if which nginx > /dev/null 2>&1; then
         install -Dm644 files/phpbeanstalkdadmin.nginx.conf \
             /etc/nginx/sites-enabled/phpbeanstalkdadmin.conf
