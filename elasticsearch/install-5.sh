d7ba75868d3987bec673a0fc81c407b618ffcfcb
Ike Devolder <ike.devolder@studioemma.eu>
2018-06-13 09:58:09 +0200

    add option to install elasticsearch version 2, 5 and 6
    Signed-off-by: Ike Devolder <ike.devolder@studioemma.eu>

---
+++ elasticsearch/install-5.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+elastic_basedir=$(dirname $(readlink -f $0))
+elastic_calldir=$(pwd)
+
+cd "$elastic_basedir"
+
+set -e
+
+wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | apt-key add -
+echo "deb https://artifacts.elastic.co/packages/5.x/apt stable main" > /etc/apt/sources.list.d/elastic-5.x.list
+apt-get update
+apt-get install elasticsearch
+
+systemctl enable elasticsearch.service
+systemctl restart elasticsearch.service
+
+cd "$elastic_calldir"
