eda78ac613a1e1991ba7bfd4cbf3381e3cf165cf
Ike Devolder <ike.devolder@studioemma.eu>
2018-06-13 09:57:17 +0200

    use nodejs 8 lts, not deprecated 7
    Signed-off-by: Ike Devolder <ike.devolder@studioemma.eu>

--- nodejs/install.sh
+++ nodejs/install.sh
@@ -7,7 +7,7 @@ cd "$nodejs_basedir"
 set -e
 
 # install nodejs
-curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
 cd "$nodejs_calldir"
