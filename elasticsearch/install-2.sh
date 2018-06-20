90b49130d90abd35deb83902febeb69ef54e1a21
Ike Devolder <ike.devolder@studioemma.eu>
2018-06-20 09:25:05 +0200

    update elasticsearch 2 install + add elastichq as ui
    Signed-off-by: Ike Devolder <ike.devolder@studioemma.eu>

--- elasticsearch/install-2.sh
+++ elasticsearch/install-2.sh
@@ -6,12 +6,32 @@ cd "$elastic_basedir"
 
 set -e
 
-wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | apt-key add -
-echo "deb https://artifacts.elastic.co/packages/2.x/apt stable main" > /etc/apt/sources.list.d/elastic-2.x.list
+wget -qO - https://packages.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+echo "deb http://packages.elastic.co/elasticsearch/2.x/debian stable main" | sudo tee -a /etc/apt/sources.list.d/elasticsearch-2.x.list
+#wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | apt-key add -
+#echo "deb https://artifacts.elastic.co/packages/2.x/apt stable main" > /etc/apt/sources.list.d/elastic-2.x.list
 apt-get update
-apt-get install elasticsearch
+apt-get install -y elasticsearch python3-pip
 
 systemctl enable elasticsearch.service
 systemctl restart elasticsearch.service
+
+(
+    cd /var/www
+    git clone https://github.com/ElasticHQ/elasticsearch-HQ.git elastichq
+    cd elastichq
+    pip3 install -r requirements.txt
+)
+chown vagrant:vagrant -R /var/www/elastichq
+install -Dm644 files/elastichq.systemd.service \
+    /etc/systemd/system/elastichq.service
+systemctl enable elastichq.service
+systemctl restart elastichq.service
+
+if which nginx > /dev/null 2>&1; then
+    install -Dm644 files/elastichq.nginx.conf \
+        /etc/nginx/sites-enabled/elastichq.conf
+    systemctl restart nginx
+fi
 
 cd "$elastic_calldir"
--- elasticsearch/install-6.sh
+++ elasticsearch/install-6.sh
@@ -9,9 +9,27 @@ set -e
 wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | apt-key add -
 echo "deb https://artifacts.elastic.co/packages/6.x/apt stable main" > /etc/apt/sources.list.d/elastic-6.x.list
 apt-get update
-apt-get install elasticsearch
+apt-get install -y elasticsearch python3-pip
 
 systemctl enable elasticsearch.service
 systemctl restart elasticsearch.service
+
+(
+    cd /var/www
+    git clone https://github.com/ElasticHQ/elasticsearch-HQ.git elastichq
+    cd elastichq
+    pip3 install -r requirements.txt
+)
+chown vagrant:vagrant -R /var/www/elastichq
+install -Dm644 files/elastichq.systemd.service \
+    /etc/systemd/system/elastichq.service
+systemctl enable elastichq.service
+systemctl restart elastichq.service
+
+if which nginx > /dev/null 2>&1; then
+    install -Dm644 files/elastichq.nginx.conf \
+        /etc/nginx/sites-enabled/elastichq.conf
+    systemctl restart nginx
+fi
 
 cd "$elastic_calldir"
