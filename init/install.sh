f67d7e773e16ad7fd2541ca57642630821eff488
Ike Devolder <ike.devolder@studioemma.eu>
2019-04-01 10:56:01 +0200

    disable apt-daily to avoid conflicts with vagrant plugins installing stuff
    Signed-off-by: Ike Devolder <ike.devolder@studioemma.eu>

--- init/install.sh
+++ init/install.sh
@@ -17,4 +17,9 @@ dpkg-reconfigure --frontend noninteractive tzdata 2>&1
 # default packages
 apt-get install -y vim htop curl git-core ant nfs-common portmap
 
+# disable apt-daily, this causes issues with vagrant some vagrant plugins
+systemctl disable apt-daily-upgrade.timer
+systemctl disable apt-daily.timer
+rm /etc/cron.daily/apt-compat
+
 cd "$init_calldir"
