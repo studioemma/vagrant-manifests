3ba34fc19d6c59ac765b1c5ca572ae3c6de1c289
Ike Devolder <ike.devolder@studioemma.eu>
2019-04-01 11:37:46 +0200

    disable more services not needed for our purpose
    Signed-off-by: Ike Devolder <ike.devolder@studioemma.eu>

--- init/install.sh
+++ init/install.sh
@@ -18,8 +18,17 @@ dpkg-reconfigure --frontend noninteractive tzdata 2>&1
 apt-get install -y vim htop curl git-core ant nfs-common portmap
 
 # disable apt-daily, this causes issues with vagrant some vagrant plugins
-systemctl disable apt-daily-upgrade.timer
-systemctl disable apt-daily.timer
+systemctl disable --now apt-daily-upgrade.timer
+systemctl disable --now apt-daily.timer
 rm /etc/cron.daily/apt-compat
+
+# disable unneeded services (for our purpose)
+systemctl mask --now lxcfs.service lxd-containers.service lxd.socket
+systemctl mask --now unattended-upgrades.service
+systemctl mask --now snapd.autoimport.service snapd.core-fixup.service \
+    snapd.seeded.service snapd.service snapd.snap-repair.timer \
+    snapd.socket snapd.system-shutdown.service
+systemctl mask --now lvm2-lvmetad.socket lvm2-lvmpolld.socket \
+    lvm2-monitor.service
 
 cd "$init_calldir"
