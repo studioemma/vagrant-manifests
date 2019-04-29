5aa041bc9a6d770b4d5fc36a1422ebbb530139d2
Ike Devolder <ike.devolder@studioemma.eu>
2019-04-29 14:12:23 +0200

    do not write nginx logs in a shared folder
    Signed-off-by: Ike Devolder <ike.devolder@studioemma.eu>

--- nginx/install.sh
+++ nginx/install.sh
@@ -10,7 +10,8 @@ set -e
 apt-get install -y nginx
 
 # create log folder in case it does not exist
-mkdir -p /var/www/website/var/log
+mkdir -p /var/log/nginx/website/
+chown vagrant:vagrant -R /var/log/nginx/website/
 
 # install config
 rm -f /etc/nginx/sites-enabled/*
